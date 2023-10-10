b468c5ac051aa632d20b34e401a9e4c13fbe197b
georgearmando <georgearmando54@gmail.com>
2023-10-10 16:49:33 +0100

    create user repository interface

---
+++ src/main/java/ao/com/georgearmando/todolist/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package ao.com.georgearmando.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+  UserModel findByUsername(String username);
+}
