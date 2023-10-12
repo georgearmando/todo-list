11e6d81f1d1c070ff0b03670bc0040d93f9c85a5
georgearmando <georgearmando54@gmail.com>
2023-10-12 18:20:00 +0100

    add list and update task method

--- src/main/java/ao/com/georgearmando/todolist/task/TaskController.java
+++ src/main/java/ao/com/georgearmando/todolist/task/TaskController.java
@@ -1,10 +1,22 @@
 package ao.com.georgearmando.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ao.com.georgearmando.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -13,8 +25,43 @@ public class TaskController {
   private ITaskRepository taskRepository;
   
   @PostMapping
-  public TaskModel create(@RequestBody TaskModel taskModel) {
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
+    taskModel.setUserId((UUID) userId);
+
+    var currentDate = LocalDateTime.now();
+    if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body("A data de inicio / termino deve ser maior que a data atual");
+    }
+
+    if(taskModel.getStartAt().isAfter(taskModel.getEndAt()) || 
+      taskModel.getStartAt().isEqual(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body("A data de inicio deve ser menor que a data de termino");
+    }
+
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
+  }
+
+  @GetMapping("/list")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
+    var tasks = this.taskRepository.findByUserId((UUID) userId);
+    return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(
+    @RequestBody TaskModel taskModel, 
+    @PathVariable UUID id, 
+    HttpServletRequest request) 
+  {
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
   }
 }
